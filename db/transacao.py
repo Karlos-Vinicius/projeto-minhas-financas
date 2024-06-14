5493091555539a5f96b92be6d0666392a7470f6b
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-14 12:30:36 -0300

    implementação da visualização da tabela pt. 1

---
+++ db/transacao.py
@@ -0,0 +1,7 @@
+TRANSACTIONS = [
+    {"categoria": "mesada", "descricao": "Ganho todo mês quando me comporto", "valor": 200, "momento": "23/01/2024 12:56"},
+    {"categoria": "mesada", "descricao": "Ganho todo mês quando me comporto", "valor": 200, "momento": "23/01/2024 12:56"},
+    {"categoria": "mesada", "descricao": "Ganho todo mês quando me comporto", "valor": 200, "momento": "23/01/2024 12:56"},
+    {"categoria": "mesada", "descricao": "Ganho todo mês quando me comporto", "valor": 200, "momento": "23/01/2024 12:56"},
+    {"categoria": "mesada", "descricao": "Ganho todo mês quando me comporto", "valor": 200, "momento": "23/01/2024 12:56"},
+]
--- routes/transactions.py
+++ routes/transactions.py
@@ -3,6 +3,10 @@ from flask import Blueprint, render_template, request
 
 transaction = Blueprint("transaction", __name__)
 
+@transaction.route("/")
+def index(): 
+    return render_template("transactions.html")
+
 @transaction.route("/cadastrar_transacao", methods=["POST", "GET"])
 def cadastrar_transacao():
     return render_template("cadastrar_transacao.html")
