03f90cd73cad587ecefd9414ae1a2cd6c14e1d17
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-14 21:45:34 -0300

    Adicionando novas transações e categorias

--- routes/transactions.py
+++ routes/transactions.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from datetime import datetime
 
 
 from db.transacao import TRANSACTIONS
@@ -11,16 +12,35 @@ transaction = Blueprint("transaction", __name__)
 def index(): 
     return render_template("transactions.html", transactions=TRANSACTIONS)
 
+
 @transaction.route("/cadastrar_transacao", methods=["POST", "GET"])
 def cadastrar_transacao():
     if request.method == "GET":
         return render_template("cadastrar_transacao.html", categorias=CATEGORIAS)
     
+    transacao = request.json
+
+    if not (transacao["valor"] and transacao["categoria"]): 
+        return render_template("cadastrar_transacao.html", categorias=CATEGORIAS)
+    
+    t = {"categoria": transacao["categoria"], "descricao": transacao["descricao"], "valor": transacao["valor"], "momento": datetime.now()}
+
+    TRANSACTIONS.append(t)
+
     return redirect(url_for("transaction.index", transactions=TRANSACTIONS))
+
 
 @transaction.route("/cadastrar_categoria", methods=["GET", "POST"])
 def cadastrar_categoria():
     if request.method == "GET":
         return render_template("cadastrar_categoria.html")
+    
+    categoria = request.json
+
+    if not categoria:
+        return render_template("cadastrar_categoria.html")
+    
+    c = categoria["categoria"]
+    CATEGORIAS.append(c)
     
     return redirect(url_for("transaction.cadastrar_transacao"))
